5a9a8385441d421f4f0c61461156f983cc5ecc02
anil-appari007 <anil.appari007@gmail.com>
2024-12-22 15:10:41 +0530

    Added state variables to function component

--- frontend/src/components/HelloWorld2.js
+++ frontend/src/components/HelloWorld2.js
@@ -1,10 +1,25 @@
-import react,{Fragment} from "react";
+import react,{Fragment, useState} from "react";
 import "../css/App.css"
 function HelloWorld2(props){
+    const [isTrue, setIsTrue] = useState(false)
+    const toggleTrue = () =>{
+        if (isTrue){
+            setIsTrue(false)
+            return
+        }
+        setIsTrue(true)
+    }
     return(
         <Fragment>
             {/* <h1 className="h1-green">HelloWorld2 from jsx....</h1> */}
             <h1 className="h1-green">{props.msg}</h1>
+            {isTrue &&
+                <Fragment>
+                    <p1>render on true</p1>
+                </Fragment>
+            }
+            <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>Toggle isTrue</a>
+
         </Fragment>
     )
 }
