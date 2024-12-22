66bda8c5d731a3e89cdcbb13e6d825908dd23920
anil-appari007 <anil.appari007@gmail.com>
2024-12-22 22:36:29 +0530

    added reusable input form component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,12 @@
-import react,{Fragment, useState} from "react";
+import react,{Fragment, useEffect, useState} from "react";
 import "../css/App.css"
+import Input from "./Input";
 function App(props){
-    const [isTrue, setIsTrue] = useState(false)
+    const [isTrue, setIsTrue] = useState(false);
+    const [crowd, setCrowd] = useState([]);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [dob, setDob] = useState("");
     const toggleTrue = () =>{
         if (isTrue){
             setIsTrue(false)
@@ -9,6 +14,25 @@ function App(props){
         }
         setIsTrue(true)
     }
+    useEffect(() =>{
+        console.log("useEffect started");
+        let people = [
+            {
+                id: 1,
+                firstName: "aaa",
+                lastName: "alll",
+                dob: "2024-12-22"
+            },
+            {
+                id: 2,
+                firstName: "bbbb",
+                lastName: "blll",
+                dob: "2024-12-23"
+            }
+        ]
+        setCrowd(people);
+    },[]);
+
     return(
         <Fragment>
             {/* <h1 className="h1-green">HelloWorld2 from jsx....</h1> */}
@@ -19,6 +43,44 @@ function App(props){
                 </Fragment>
             }
             <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>Toggle isTrue</a>
+
+            <hr/>
+            <Input
+                title="first name"
+                type="text"
+                name="first-name-new"
+                className="form-control"
+                onChange={(event) => setFirstName(event.target.value)}
+                >
+            </Input>
+            <Input
+                title="last name"
+                type="text"
+                name="last-name-new"
+                className="form-control"
+                onChange={(event) => setLastName(event.target.value)}
+                >
+                </Input>
+            <Input
+                title="dob"
+                type="date"
+                name="dob-new"
+                className="form-control"
+                onChange={(event) => setDob(event.target.value)}
+            ></Input>
+
+            <div>
+                firstName: {firstName}<br/>
+                lastName: {lastName}<br/>
+                dob: {dob}<br/>
+            </div>
+            <hr/>
+            <h4>People</h4>
+            <ul className="list-group">
+                {crowd.map((m) =>(
+                    <li key={m.id} className="list-group-item">{m.firstName} {m.lastName}</li>
+                ))}
+            </ul>
 
         </Fragment>
     )
