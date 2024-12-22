7550f823da02b3f4a3c818b9a9d8c02acfa56406
anil-appari007 <anil.appari007@gmail.com>
2024-12-22 23:59:06 +0530

    added forwardref to inputs

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import react,{Fragment, useEffect, useState} from "react";
+import react,{Fragment, useEffect, useRef, useState} from "react";
 import "../css/App.css"
 import Input from "./Input";
 function App(props){
@@ -7,6 +7,11 @@ function App(props){
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [dob, setDob] = useState("");
+
+    const firstNameRef = useRef(null);
+    const lastNameRef = useRef(null);
+    const dobRef = useRef(null);
+
     const toggleTrue = () =>{
         if (isTrue){
             setIsTrue(false)
@@ -33,6 +38,34 @@ function App(props){
         setCrowd(people);
     },[]);
 
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        console.log(firstName, lastName, dob)
+
+        if (firstName != ""){
+            addPerson(firstName, lastName, dob)
+        }
+    }
+    const addPerson = (newfirstName, newlastName, newdob)=>{
+        let newPerson = {
+            id: crowd.length +1,
+            firstName: newfirstName,
+            lastName: newlastName,
+            dob: newdob,
+        }
+
+        const newCrowd = crowd.concat(newPerson);
+        setCrowd(newCrowd)
+        setFirstName("");
+        setLastName("");
+        setDob("");
+
+        //
+        firstNameRef.current.value = "";
+        lastNameRef.current.value = "";
+        dobRef.current.value = "";
+    }
+
     return(
         <Fragment>
             {/* <h1 className="h1-green">HelloWorld2 from jsx....</h1> */}
@@ -45,10 +78,12 @@ function App(props){
             <a href="#!" className="btn btn-outline-secondary" onClick={toggleTrue}>Toggle isTrue</a>
 
             <hr/>
+            <form autoComplete="off" onSubmit={handleSubmit}>
             <Input
                 title="first name"
                 type="text"
                 name="first-name-new"
+                ref={firstNameRef}
                 className="form-control"
                 onChange={(event) => setFirstName(event.target.value)}
                 >
@@ -57,6 +92,7 @@ function App(props){
                 title="last name"
                 type="text"
                 name="last-name-new"
+                ref={lastNameRef}
                 className="form-control"
                 onChange={(event) => setLastName(event.target.value)}
                 >
@@ -65,9 +101,13 @@ function App(props){
                 title="dob"
                 type="date"
                 name="dob-new"
+                ref={dobRef}
                 className="form-control"
                 onChange={(event) => setDob(event.target.value)}
             ></Input>
+            <input type="submit" value="submit" className="btn btn-primary"></input>
+            </form>
+            
 
             <div>
                 firstName: {firstName}<br/>
