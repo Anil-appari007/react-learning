bf9be35cdef71c785d1879d8f38a160040c5458e
anil-appari007 <anil.appari007@gmail.com>
2024-12-22 14:33:57 +0530

    Added Helloworld in jsx

---
+++ frontend/src/components/HelloWorld2.js
@@ -0,0 +1,11 @@
+import react,{Fragment} from "react";
+
+function HelloWorld2(){
+    return(
+        <Fragment>
+            <h1>HelloWorld2 from jsx....</h1>
+        </Fragment>
+    )
+}
+
+export default HelloWorld2
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HelloWorld from './components/HelloWorld';
+import HelloWorld2 from './components/HelloWorld2';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HelloWorld />
+    <HelloWorld2 />
   </React.StrictMode>
 );
 
